a733f9ac19727e79911b657fa3e33863524a97dd
Stolarek <przemyslaw.stolarek1@gmail.com>
2023-05-29 16:56:19 +0200

    Add variants to TextField, style TextField, expand TextField interface

--- react-custom-components/src/App.tsx
+++ react-custom-components/src/App.tsx
@@ -2,11 +2,21 @@ import { TextField } from './components/TextField/TextField';
 import { useState } from 'react';
 function App() {
   const [text, setText] = useState('');
-
+  const style = { backgroundColor: 'red', height: '100px', padding: '10px' };
   return (
-    <>
+    <div>
+      <TextField
+        value={text}
+        onChange={e => setText(e.target.value)}
+        variant='filled'
+      />
       <TextField value={text} onChange={e => setText(e.target.value)} />
-    </>
+      <TextField
+        value={text}
+        onChange={e => setText(e.target.value)}
+        variant='outlined'
+      />
+    </div>
   );
 }
 
--- react-custom-components/src/components/TextField/TextField.tsx
+++ react-custom-components/src/components/TextField/TextField.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 import './TextField.scss';
-
 interface TextFieldProps {
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   disabled?: boolean;
   placeholder?: string;
+  required?: boolean;
+  size?: 'small' | 'medium' | 'large';
+  variant?: 'filled' | 'outlined' | 'standard';
+  color?:
+    | 'primary'
+    | 'secondary'
+    | 'error'
+    | 'info'
+    | 'success'
+    | 'warning'
+    | string;
 }
 
 export const TextField = ({
@@ -13,6 +23,10 @@ export const TextField = ({
   onChange,
   disabled,
   placeholder,
+  size,
+  required,
+  variant,
+  color,
 }: TextFieldProps) => {
   const [isFocused, setIsFocused] = useState(false);
 
@@ -24,19 +38,60 @@ export const TextField = ({
     setIsFocused(false);
   };
 
+  const handleVariant = (
+    variant: 'filled' | 'outlined' | 'standard' | undefined
+  ) => {
+    if (variant === 'filled') return 'filled';
+    if (variant === 'outlined') return 'outlined';
+    return 'standard';
+  };
+
+  const handleColor = (
+    color:
+      | 'primary'
+      | 'secondary'
+      | 'error'
+      | 'info'
+      | 'success'
+      | 'warning'
+      | string
+      | undefined
+  ): string => {
+    switch (color) {
+      case 'secondary':
+        return 'secondary';
+      case 'error':
+        return 'error';
+      case 'info':
+        return 'info';
+      case 'success':
+        return 'success';
+      case 'warning':
+        return 'warning';
+      case 'primary':
+        return 'primary';
+
+      default:
+        return 'primary';
+    }
+  };
+
   return (
     <div className={`text-field ${isFocused || value ? 'active' : ''}`}>
-      <input
-        type='text'
-        className='standard'
-        value={value}
-        onChange={onChange}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        placeholder={placeholder}
-        disabled={disabled}
-      />
-      <label>Label</label>
+      <div className={handleColor(color)}>
+        <input
+          type='text'
+          className={handleVariant(variant)}
+          value={value}
+          onChange={onChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          placeholder={placeholder}
+          disabled={disabled}
+          required={required}
+        />
+        <label>Label</label>
+      </div>
     </div>
   );
 };
